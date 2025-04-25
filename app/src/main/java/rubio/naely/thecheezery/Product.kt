fa0325e146c2e56924062b13f9134c74374d701e
Naely Rubio <guimelrubio021qgmail.com>
2025-04-24 18:33:19 -0700

    Primera implementación de ListView, productos y navegación

---
+++ app/src/main/java/rubio/naely/thecheezery/Product.kt
@@ -0,0 +1,7 @@
+package rubio.naely.thecheezery
+
+data class Product (var name: String,
+                    var image: Int,
+                    var description: String,
+                    var price: Double)
+
