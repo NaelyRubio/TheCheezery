95c0ea2c4ec869244b627c04b2cfdf43e912d199
Naely Rubio <guimelrubio021qgmail.com>
2025-04-25 20:53:36 -0700

    Diseño de productos por categoría con imagen dinámica, botón de regreso y estilo mejorado

--- app/src/main/java/rubio/naely/thecheezery/MenuActivity.kt
+++ app/src/main/java/rubio/naely/thecheezery/MenuActivity.kt
@@ -2,9 +2,9 @@ package rubio.naely.thecheezery
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -15,12 +15,35 @@ class MenuActivity : AppCompatActivity() {
         setContentView(R.layout.activity_menu)
 
 
-        var btnCold: Button = findViewById(R.id.button_cold_drinks) as Button
+        val coldButton = findViewById<AppCompatButton>(R.id.button_cold_drinks)
+        val hotButton = findViewById<AppCompatButton>(R.id.button_hot_drinks)
+        val sweetsButton = findViewById<AppCompatButton>(R.id.button_sweets)
+        val saltiesButton = findViewById<AppCompatButton>(R.id.button_salties)
 
-        btnCold.setOnClickListener{
-            var intent: Intent = Intent (this, ProductosActivity::class.java)
-            startActivity(intent)
+        coldButton.setOnClickListener {
+            abrirCategoria("coldDrinks")
         }
 
+        hotButton.setOnClickListener {
+            abrirCategoria("hotDrinks")
+        }
+
+        sweetsButton.setOnClickListener {
+            abrirCategoria("sweets")
+        }
+
+        saltiesButton.setOnClickListener {
+            abrirCategoria("salties")
         }
     }
+
+    private fun abrirCategoria(categoria: String) {
+        val intent = Intent(this, ProductosActivity::class.java)
+        intent.putExtra("categoria", categoria)
+        startActivity(intent)
+    }
+
+}
+
+
+
